9613f2bd8aa25223d806230e6aa5b116e0bb0358
Marcus Kida <marcus.kida@bearologics.com>
2020-10-31 19:23:52 +0100

    Use DO Docker Registry

--- scripts/build-push-registry.sh
+++ scripts/build-push-registry.sh
@@ -2,6 +2,6 @@
 
 VERSION=$(git describe --tags)
 
-docker build -t registry.bearologics.cloud/xcodereleases-telegram-bot:$VERSION .
+docker build -t registry.digitalocean.com/bearologics-cloud/xcodereleases-telegram-bot:$VERSION .
 
-docker push registry.bearologics.cloud/xcodereleases-telegram-bot:$VERSION
+docker push registry.digitalocean.com/bearologics-cloud/xcodereleases-telegram-bot:$VERSION
